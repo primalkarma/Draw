d1d03ac2ea2d1340714f1bba52c6e77af9d82cc9
primalkarma <primalkarma@gmail.com>
2025-07-15 08:36:41 +0530

    Initial Commit

---
+++ Draw/DrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawApp.swift
+//  Draw
+//
+//  Created by Parimal Devi on 15/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
